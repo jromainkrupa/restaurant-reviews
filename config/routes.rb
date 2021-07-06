7af83c2b316e71d20fb7c3e660eb9216a04c861f
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-07-06 19:56:11 +0200

    livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :restaurants do
+  #   collection do
+  #     get :top
+  #   end
+    
+  #   member do 
+  #     get :chef
+  #   end
+  # end
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    # GET restaurants/42/reviews/new
+  end
+
+  resources :reviews, only: [:show]
+
+  # "restaurants/top"
+  # get "/top", to: "resturants#top"
+
+  # GET /restaurants/42/chef
+  
 end
